d87c58c7938b13122a3c84bd7e30a632b95a5319
alex <alexclass2015@gmail.com>
2017-11-21 22:38:10 -0500

    fixed initial guassian to be normalized. Finicky for different initial speeds

--- tdse.py
+++ tdse.py
@@ -7,8 +7,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import plotutil
 
 def gauss(x):
-    k0 = 5
-    return np.exp(-.25*(k0**2))*np.exp(-((x-(k0*1j))**2))
+    return ((np.pi/2)**(-1/4))*np.exp(-(x**2) + 1j*x)
 
 def Bdirichlet(q):
     q[0] = 0
@@ -63,37 +62,15 @@ def init():
 
 
 def TDSE():
-    """
-    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter )
-    parser.add_argument("J", type int,
-                        help="number of spatial support points")
-    parser.add_argument("dt0",type=float,
-                        help="timeste")
-    args = parser.parse_args()
-    J = args.J
 
-    fBNC = Bdirichlet
-    fINC = free
-
-    minmaxx = np.array([-1.0,1.0])
-    minmaxt = np.array([0.0, 1.0])
-    N = int((minmaxt[1]-minmaxt[0])/dt0+1
-    dx = (minmaxx[1]-minmaxx[0])/float(J-1)
-    dt = (minmaxt[1]-minmaxt[0])/float(N-1)
-    x = minmaxx[0]+np.arrange(J)
-    t = minmaxt[0]+np.arange(N)*dt
-    alpha = (1j)*dt/(dx**2)
-
-    y = cranknicholson(x,t,alpha,fBNC,fINC)
-    """
     fBNC = Bdirichlet
     fINC = gauss
     fPOT = free
     kappa = 1j
-    #hbar/2m = 1
-    J = 200
-    N = 200
-    minmaxx = np.array([-20,20])
+    # hbar/2m = 1
+    J = 500
+    N = 500
+    minmaxx = np.array([-50, 50])
     minmaxt = np.array([0.0, 5.0])
     dx = (minmaxx[1]-minmaxx[0])/float(J-1)
     dt = (minmaxt[1]-minmaxt[0])/float(N-1)
@@ -108,10 +85,16 @@ def TDSE():
 
 def main():
     x, t, q = TDSE()
-    plt.plot(x,np.abs(q[:,0]))
-    plt.plot(x,np.abs(q[:,5]))
-    plt.plot(x,np.abs(q[:,30]))
-    plt.plot(x,np.abs(q[:,50]))
+    q = np.abs(q)
+
+    #fig = plt.figure(num=1,figsize=(8,8),dpi=100,facecolor='white')
+    #ax = fig.add_subplot(111,projection='3d')
+    #t2d,x2d = np.meshgrid(t,x)
+    #ax.plot_surface(x2d,t2d,q)
+    plt.plot(x,q[:,0])
+    plt.plot(x,q[:,10])
+    plt.plot(x,q[:,50])
+    plt.plot(x,q[:,100])
     plt.show()
 
 main()
